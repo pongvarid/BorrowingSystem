d4ea3102fe35ca1d663c0420727c9b69fcb3227f
พงษ์วริษฐ์ มณีวรรณ์ <pongvarid@gmail.com>
2019-11-20 05:01:22 +0700

    login and logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,127 +3,110 @@
 @section('content')
 
 
-<div class="w3-cell-row" style="height:100vh">
-    <div class="w3-container box-green w3-cell w3-cell-middle ">
-     
-            <div class="mrl-60">
-                <h3 class="w3-padding-64 wh w3-left"><b class="fs18">ระบบ<br><b class="fs30">ยืมคืนพัสดุของสงฆ์</b></h3>
-            </div>
- 
+<div class="w3-cell-row">
+    <div class="w3-container box-green w3-cell w3-cell-middle shadow" style="height:100vh">
+
+        <h3 class="mrl-60 wh w3-left w3-text-white"><b class="fs28">ระบบ</b><br><b class="fs40">ยืมคืนพัสดุของสงฆ์</b>
+        </h3>
+
     </div>
-    <div class="w3-container w3-cell w3-cell-middle">
+
+    <div class="w3-container w3-center w3-cell w3-cell-middle wal" style="height:100vh">
+
         <center>
-            <div style="width:60%;">
-
-                <div class="w3-card-4">
-                    <div class="w3-container w3-green">
-                        <h2>Header</h2>
-                    </div>
-
-                    <form class="w3-container">
-
-                        <label>First Name</label>
-                        <input class="w3-input" type="text">
-
-                        <label>Last Name</label>
-                        <input class="w3-input" type="text">
-
-                    </form>
-
+            <div class="w3-card-4 w3-round-xxlarge w3-white" style="width:550px;">
+                <div class="w3-container w3-text-green w3-round-xxlarge">
+                    <hr class="w3-text-green w3-green">
+                    <h2 class="w3-text-green">สมาชิก</h2>
+                    <hr class="w3-text-green w3-green">
                 </div>
+                <!----  <form class="w3-container pd-20" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <span class="w3-left w3-text-orange">
+
+                    </span>
+                    <input class=" fs18 w3-input w3-border round"  name="email"   placeholder="ชื่อผู้ใช้" type="text"><br>
+
+                    <input class="fs18 w3-input w3-border round" type="password" name="password" placeholder="รหัสผ่าน" type="text"><br>
+                    <button
+                        class="w3-button w3-white box-green  w3-round-large round fs18 w3-text-white">เข้าสู่ระบบ</button>
+
+                </form> --->
+
+                <form method="POST" action="{{ route('login') }}" class="pd-40">
+                    @csrf
+                    <input id="email" type="email"
+                        class="fs18 w3-input w3-border round @error('email') is-invalid @enderror" name="email"
+                        placeholder="ชื่อผู้ใช้"
+                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                    <input id="password" type="password"
+                    placeholder="รหัสผ่าน"
+                        class="fs18 mrt-20 w3-input w3-border round @error('password') is-invalid @enderror" name="password"
+                        required autocomplete="current-password">
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+
+                    <div class="form-group row w3-left ">
+                        <div class="col-md-6 offset-md-4 ">
+                            <div class="form-check ">
+                                    <input class="form-check-input mrl-10 mrt-10" type="checkbox" name="remember" id="remember"
+                                    {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <br> <br>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit"
+                                class="w3-button w3-white box-green  w3-round-large round fs18 w3-text-white" style="width:60%;">
+                                {{ __('Login') }}
+                            </button> <br> <br>
+
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                            @endif
+                        </div>
+                    </div>
+                </form>
+
+                <br>
 
             </div>
         </center>
-
     </div>
+
+
 </div>
 
-
-
-<!----
-<div class="container-fluid">
-    <div class="row align-items-center"> 
-        <div class="col-xs-12 col-md-6">
-
-        </div>
-        <div class="col-xs-12 col-md-6 justify-content-center">
-            <div class="card ">
-                <div class="card-header">{{ __('Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
---->
-
+ 
 
 <style>
     .box-green {
         background: rgb(39, 201, 107);
         background: linear-gradient(90deg, rgba(39, 201, 107, 1) 0%, rgba(15, 193, 155, 1) 100%);
+    }
+
+    .wal{
+        background: #5c4242 url(http://p3.isanook.com/tr/0/wb/i/url/p1.s1sf.com/tr/0//ui/187/937457/anu_9414-2-2_1306393083.jpg;c:width=1024,height=768,maintain_ratio=1;static:p_s1sf_tr_0;file:13750a.jpg) no-repeat left top;
+background-size: cover;
     }
 </style>
 
