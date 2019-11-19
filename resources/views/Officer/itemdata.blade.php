c5cb3277d503a0369cba11bc6ece85c68292de05
Nuthhapong Patchotchai <flooka_kung@hotmail.com>
2019-11-20 06:31:50 +0700

    multi

--- resources/views/Officer/itemdata.blade.php
+++ resources/views/Officer/itemdata.blade.php
@@ -39,7 +39,6 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="table-responsive col-md-12">
-<<<<<<< HEAD
                         <table id="mytable" class="table table-bordred table-striped">
                             <thead>
                                 <th>รูปภาพ</th>
@@ -53,21 +52,6 @@
                             </thead>
                             <tbody>
                                 <tr>
-=======
-                            <table id="mytable" class="w3-table-all">
-                                    <thead>
-                                    <th>รูปภาพ</th>
-                                    <th>ประเภทพัสดุ</th>
-                                    <th>ชื่อ</th>
-                                    <th>รายละเอียด</th>
-                                    <th>จำนวน</th>
-                                    <th>คงเหลือ</th>
-                                    <th>แก้ไข</th>
-                                    <th>ลบ</th>
-                                    </thead>
-                                <tbody>
-                                    <tr>
->>>>>>> d4df741b2506e95985181fd1e5d7834cf13c0eda
                                     <td>รูปขนาด 50*50px</td>
                                     <td>เครื่องใช้ในครัว</td>
                                     <td>จาน</td>
@@ -78,15 +62,14 @@
                                     <td><a class="w3-button w3-white w3-border w3-border-red w3-round-large" href="#">ลบ</a></td>
                                 </tr>
                                 <tr>
-<<<<<<< HEAD
                                     <td>รูปขนาด 50*50px</td>
                                     <td>เครื่องใช้ในครัว</td>
                                     <td>ซ้อม</td>
                                     <td>*******</td>
                                     <td>300</td>
                                     <td>100</td>
-                                    <td><a class="btn btn-warning" href="/edititemdata">แก้ไข</a></td>
-                                    <td><a class="btn btn-danger" href="#">ลบ</a></td>
+                                    <td><a class="w3-button w3-white w3-border w3-border-yellow w3-round-large" href="/edititemdata">แก้ไข</a></td>
+                                    <td><a class="w3-button w3-white w3-border w3-border-red w3-round-large" href="#">ลบ</a></td>
                                 </tr>
                                 <tr>
                                     <td>รูปขนาด 50*50px</td>
@@ -95,33 +78,11 @@
                                     <td>*******</td>
                                     <td>200</td>
                                     <td>100</td>
-                                    <td><a class="btn btn-warning" href="/edititemdata">แก้ไข</a></td>
-                                    <td><a class="btn btn-danger" href="#">ลบ</a></td>
+                                    <td><a class="w3-button w3-white w3-border w3-border-yellow w3-round-large" href="/edititemdata">แก้ไข</a></td>
+                                    <td><a class="w3-button w3-white w3-border w3-border-red w3-round-large" href="#">ลบ</a></td>
                                 </tr>
                             </tbody>
                         </table>
-=======
-                                        <td>รูปขนาด 50*50px</td>
-                                        <td>เครื่องใช้ในครัว</td>
-                                        <td>ซ้อม</td>
-                                        <td>*******</td>
-                                        <td>300</td>
-                                        <td>100</td>
-                                        <td><a class="w3-button w3-white w3-border w3-border-yellow w3-round-large" href="/edititemdata">แก้ไข</a></td>
-                                        <td><a class="w3-button w3-white w3-border w3-border-red w3-round-large" href="#">ลบ</a></td>
-                                </tr>
-                                    <tr>
-                                            <td>รูปขนาด 50*50px</td>
-                                            <td>เครื่องใช้ในครัว</td>
-                                            <td>ช้อน</td>
-                                            <td>*******</td>
-                                            <td>200</td>
-                                            <td>100</td>
-                                            <td><a class="w3-button w3-white w3-border w3-border-yellow w3-round-large" href="/edititemdata">แก้ไข</a></td>
-                                            <td><a class="w3-button w3-white w3-border w3-border-red w3-round-large" href="#">ลบ</a></td>
-                                </tr>
-                                </tbody>
-                    </table>
 
                 <div class="clearfix"></div>
                 <ul class="pagination pull-right">
@@ -133,18 +94,6 @@
                   <li><a href="#">5</a></li>
                   <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
                 </ul>
->>>>>>> d4df741b2506e95985181fd1e5d7834cf13c0eda
-
-                        <div class="clearfix"></div>
-                        <ul class="pagination pull-right">
-                            <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
-                            <li class="active"><a href="#">1</a></li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            <li><a href="#">5</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
-                        </ul>
 
                     </div>
     </font>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,12 @@
   <link rel="stylesheet" href="./css/le/fonts.css">
   <link rel="stylesheet" href="./css/le/customize.css">
   <link rel="stylesheet" href="./css/le/fonts.css">
+  <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="/js/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
   </script>
@@ -20,7 +25,7 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
   </script>
- 
+
   <title>@yield('title')</title>
 
   <style>
@@ -28,7 +33,7 @@
 html,body,div,h1,h2,h3,h4,p,li,span,a{
     font-family: 'Sarabun', sans-serif!important;
 
-} 
+}
 .f{
     font-family: 'Sarabun', sans-serif!important;
 
@@ -53,12 +58,12 @@ html,body,div,h1,h2,h3,h4,p,li,span,a{
               document.getElementById("mySidebar").style.display = "block";
               document.getElementById("myOverlay").style.display = "block";
             }
-             
+
             function w3_close() {
               document.getElementById("mySidebar").style.display = "none";
               document.getElementById("myOverlay").style.display = "none";
             }
-            
+
             // Modal Image Gallery
             function onClick(element) {
               document.getElementById("img01").src = element.src;
