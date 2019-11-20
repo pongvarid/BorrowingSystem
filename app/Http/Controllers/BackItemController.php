4debf5977aaed63148c938b6c2bef597c9309fc9
pbenzyk <memejh0331@gmail.com>
2019-11-20 10:04:16 +0700

    update

--- app/Http/Controllers/BackItemController.php
+++ app/Http/Controllers/BackItemController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\BookItem;
-use App\BookItemDetail;
-
 use Illuminate\Http\Request;
 
-class WaitItemController extends Controller
+class BackItemController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,11 +13,7 @@ class WaitItemController extends Controller
      */
     public function index()
     {
-        $data = BookItem::join('borrower', 'book_item.brw_id', '=', 'borrower.id')
-            ->select('book_item.*', 'borrower.*')
-            ->paginate(5);
-        $list = array("รอการพิจารณา", "อนุมัติ", "ไม่อนุมัติ");
-        return view('Officer.waititem', compact('data', 'list'));
+        
     }
 
     /**
@@ -52,12 +45,7 @@ class WaitItemController extends Controller
      */
     public function show($id)
     {
-        $data = BookItemDetail::where('book_item_detail.bookitem_id', '=', $id)
-            ->leftJoin('book_item', 'book_item_detail.bookitem_id', '=', 'book_item.id')
-            ->leftJoin('item', 'book_item_detail.item_id', '=', 'item.id')
-            ->select('book_item_detail.*', 'book_item.*', 'item.item_name')
-            ->paginate(5);
-        return view('Officer.waititemdetail', ['data' => $data]);
+        //
     }
 
     /**
@@ -80,15 +68,7 @@ class WaitItemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $data = BookItem::where('id', '=', $id);
-        $data->update([
-            'bookitem_status' => $data->bookitem_status
-        ]);
-        if ($data) {
-            return redirect('waititem')->with('success', ' Success!');
-        } else {
-            return redirect('waititem')->with('error', 'Error!');
-        }
+        //
     }
 
     /**
